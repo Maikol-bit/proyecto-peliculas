962260f4a1ef3132c273d61431dec9211be647b0
Maikol-bit <226600168+Maikol-bit@users.noreply.github.com>
2025-08-14 17:33:02 -0500

    feat(Medium): :tada:

--- main.py
+++ main.py
@@ -1,2 +1,48 @@
+"""
+NETFLIX CONSOLE
+Proyecto de programación en Python
+Autor: [Maikol Delgado]
+Fecha: [14 de agosto]
+
+Descripción: Sistema de navegación de películas tipo Netflix
+usando archivos JSON y menús interactivos.
+"""
+import json
+import os
+import sys
+
+favoritas = []
+historial = []
+
 def main():
-    pass
+    """Función pirncipal del programa"""
+    print("Iniciando netflix console...")
+
+    print("Programa iniciado correctamente")
+
+if __name__ == "__main__":
+        main()
+
+
+"""
+NETFLIX CONSOLE - Carga de datos JSON
+"""
+
+import json
+import os
+
+def cargar_peliculas():
+      """
+      Cargar las Peliculas desde el archivo JSON
+
+      Returns:
+      dict: Diccionario con las peliculas organizadas por género
+      None: Si hay algun error al cargar el archivo
+      """
+      try:
+            if not os.path.exists('peliculas.json'):
+                  print("Error: No se encontro el archivo 'peliculas.json'")
+                  print("Asegurate que este en la misma carpeta que main.py")
+                  return None
+            
+            with open()
